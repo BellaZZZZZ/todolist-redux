781570f362e051f7509fcd7428f658c9ed92ef6c
BellaZZZZZ <bellazhao_321@qq.com>
2021-04-04 22:57:05 +0800

    update

---
+++ src/TodoList.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import store from './store/index.js';
+import { getInputChangeAction, getAddItemAction, getDeleteItemAction } from './store/actionCreators.js';
+import TodoListUI from './TodoListUI';
+
+// const data = [
+//   'Racing car sprays burning fuel into crowd.',
+//   'Japanese princess to wed commoner.',
+//   'Australian walks 100km after outback crash.',
+//   'Man charged over missing wedding girl.',
+//   'Los Angeles battles huge wildfires.',
+// ];
+
+class TodoList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = store.getState();
+    console.log(this.state);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleStoreChange = this.handleStoreChange.bind(this);
+    store.subscribe(this.handleStoreChange);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+  render(){
+      return(
+        <TodoListUI />
+      )
+  }
+  handleInputChange(e) {
+    const action = getInputChangeAction(e.target.value);
+    store.dispatch(action)
+  }
+
+  handleStoreChange() {
+    this.setState(store.getState());
+  }
+  handleButtonClick(){
+    const action = getAddItemAction();
+    store.dispatch(action);
+  }
+
+  handleItemDelete(index) {
+    const action = getDeleteItemAction(index);
+    store.dispatch(action);
+  }
+}
+
+export default TodoList;
---
+++ src/TodoListUI.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Input, Button, List } from 'antd';
+
+class TodoListUI extends Component {
+  render() {
+    return (
+      <div style={{marginTop: '10px', marginLeft: '10px'}}>
+        <div>
+          <Input
+            value={this.state.inputValue} 
+            placeholder='todo info' 
+            style={{width: '300px', marginRight: '10px'}}
+            onChange={this.handleInputChange}
+          />
+          <Button onClick={this.handleButtonClick} type="primary">提交</Button>
+        </div>
+        <List
+          style={{marginTop: '10px', width: '300px'}}
+          bordered
+          dataSource={this.state.list}
+          renderItem={(item, index) => (
+            <List.Item onClick={this.handleItemDelete.bind(this, index)}>
+              {item}
+            </List.Item>
+          )}
+        />
+      </div>
+    )
+  }
+}
+
+export default TodoListUI;
